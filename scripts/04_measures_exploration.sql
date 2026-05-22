4ef934ba2a59960f637e3c4268b89b92308c0e65
Abdulkarem Bawazir <abdulkarem.k.bawazir@gmail.com>
2026-05-22 18:55:27 +0800

    Add measures exploration SQL for key metrics
    Added SQL queries to calculate key metrics including total sales, total quantity, average price, total orders, total products, and total customers.

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -1,1 +1,52 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT() => Counts the number of rows or distinct values.
+    - SUM()   => Calculates the total sum of a numeric column.
+    - AVG()   => Computes the average value of a numeric column.
+    - UNION ALL => Combines results from multiple SELECT statements into a single result set.
+    - DISTINCT => Used with COUNT() to count unique values.
+===============================================================================
+*/
+
+-- Find the Total Sales
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
+
+-- Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+
+-- Find the average selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
+
+-- Find the total number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+
+-- Find the total number of customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
+
+-- Find the total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
+
+-- Generate a Report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
 
